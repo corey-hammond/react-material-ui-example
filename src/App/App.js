d8d56ba3169b15b6c713b6319664e3df6533ead3
Corey Hammond <coreyjhammond@gmail.com>
2021-03-21 21:28:58 -0700

    acleaning up; adding SideMenu component with initial styles

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SideMenu from './components/SideMenu';
+
+function App() {
+  return (
+    <>
+      <SideMenu />
+    </>
+  );
+}
+
+export default App;

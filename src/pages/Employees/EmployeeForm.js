cb2db4097d88c42782a655310237e51d9b792a1e
Corey Hammond <coreyjhammond@gmail.com>
2021-03-26 13:00:51 -0700

    finished form components

--- src/pages/Employees/EmployeeForm.js
+++ src/pages/Employees/EmployeeForm.js
@@ -2,6 +2,7 @@ import Grid from '@material-ui/core/Grid';
 import React, { useState, useEffect } from 'react';
 import Controls from '../../components/controls/Controls';
 import { useForm, Form } from '../../components/useForm';
+import * as employeeService from '../../services/employeeService';
 
 const genderItems = [
   { id: 'male', title: 'Male' },
@@ -12,7 +13,7 @@ const initialFieldValues = {
   id: 0,
   fullName: '',
   email: '',
-  mobile: '',
+  phone: '',
   city: '',
   gender: 'male',
   departmentId: '',
@@ -39,15 +40,51 @@ export default function EmployeeForm() {
             value={values.email}
             onChange={handleInputChange}
           />
+          <Controls.Input
+            name='phone'
+            label='Phone Number'
+            value={values.phone}
+            onChange={handleInputChange}
+          />
+          <Controls.Input
+            name='city'
+            label='City'
+            value={values.city}
+            onChange={handleInputChange}
+          />
         </Grid>
         <Grid item xs={6}>
+          <Controls.Checkbox
+            name='isPermanent'
+            label='Permanent Employee'
+            value={values.isPermanent}
+            onChange={handleInputChange}
+          />
           <Controls.RadioGroup
             name='gender'
             value={values.gender}
             label='Gender'
             items={genderItems}
             onChange={handleInputChange}
-          ></Controls.RadioGroup>
+          />
+          <Controls.Select
+            name='departmentId'
+            label='Department'
+            value={values.departmentId}
+            onChange={handleInputChange}
+            options={employeeService.getDepartmentCollection()}
+          />
+          <Controls.DatePicker
+            name='hireDate'
+            label='Hire Date'
+            value={values.hireDate}
+            onChange={handleInputChange}
+          />
+
+          <div>
+            <Controls.Button type='submit' text='Submit' />
+            <Controls.Button color='default' text='Reset' />
+          </div>
         </Grid>
       </Grid>
     </Form>
